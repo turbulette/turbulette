f6bb01657735b5438cb85d9271d4103ea9e4666f
Gazorby <gazorby@pm.me>
2020-08-14 15:44:46 +0200

    ✨ feat: mock headers when using Tester class

--- turbulette/test/tester.py
+++ turbulette/test/tester.py
@@ -10,25 +10,31 @@ class Tester:
         self.schema = schema
 
     async def query(
-        self, query: str, variables: dict = None, op_name: str = None
+        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
     ) -> GraphQLResult:
+
+        class TestRequest:
+            def __init__(self, headers: dict = None):
+                self.headers = headers
+
         return await graphql(
             self.schema,
             data={"query": query, "variables": variables, "operationName": op_name},
+            context_value={"request": TestRequest(headers)}
         )
 
     async def assert_query_success(
-        self, query: str, variables: dict = None, op_name: str = None
+        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
     ) -> GraphQLResult:
-        response = await self.query(query, variables, op_name)
+        response = await self.query(query, variables, op_name, headers)
         self.assert_status_200(response)
         self.assert_no_errors(response)
         return response
 
     async def assert_query_failed(
-        self, query: str, variables: dict = None, op_name: str = None
+        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
     ) -> GraphQLResult:
-        response = await self.query(query, variables, op_name)
+        response = await self.query(query, variables, op_name, headers)
         self.assert_status_200(response)
         self.assert_errors(response)
         return response
