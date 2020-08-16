627a4c146ec86d4a078851dac80bd04cc92adefc
Gazorby <gazorby@pm.me>
2020-08-16 16:20:30 +0200

    ✅ tests: add get_user_by_payload

--- tests/app_1/tests/test_auth.py
+++ tests/app_1/tests/test_auth.py
@@ -1,3 +1,4 @@
+from base64 import decode
 from collections import UserString
 from importlib import reload
 import pytest
@@ -217,7 +218,6 @@ async def test_wrong_signature(turbulette_setup, tester, false_access_jwt):
     assert not response[1]["data"]["books"]["books"]
 
 
-
 async def test_no_verify(turbulette_setup, tester, false_refresh_jwt):
     turbulette_setup.settings.configure(JWT_VERIFY=False)
     response = await tester.assert_query_success(
@@ -233,3 +233,52 @@ async def test_no_verify(turbulette_setup, tester, false_refresh_jwt):
     )
     assert not response[1]["data"]["refreshJWT"]["errors"]
     assert response[1]["data"]["refreshJWT"]["accessToken"]
+
+
+async def test_get_token_from_user(turbulette_setup, create_user, tester):
+    from tests.app_1.models import BaseUser
+    from turbulette.apps.auth import get_token_from_user
+
+    user = await BaseUser.get_by_username("test_user")
+    access_token = get_token_from_user(user)
+    # Check token is valid
+
+    response = await tester.assert_query_success(
+        query="""
+            query {
+                books {
+                    books {
+                        title
+                        author
+                    }
+                    errors
+                }
+            }
+        """,
+        headers={"authorization": f"JWT {access_token}"},
+    )
+
+    assert not response[1]["data"]["books"]["errors"]
+    assert response[1]["data"]["books"]["books"]
+
+
+async def test_get_user_by_payload(turbulette_setup, create_user, get_tokens, false_access_jwt, tester):
+    from turbulette.apps.auth.core import get_user_by_payload, decode_jwt
+    from turbulette.apps.auth.exceptions import JSONWebTokenError, UserDoesNotExists
+    from tests.app_1.models import BaseUser
+
+    claims = decode_jwt(get_tokens[1])[1]
+    user = await get_user_by_payload(claims)
+
+    assert user.username == "test_user"
+
+    # Invalid payload
+    claims["sub"] = None
+
+    with pytest.raises(JSONWebTokenError):
+        user = await get_user_by_payload(claims)
+
+    # Invalid user
+    claims = decode_jwt(false_access_jwt)[1]
+    with pytest.raises(UserDoesNotExists):
+        user = await get_user_by_payload(claims)
