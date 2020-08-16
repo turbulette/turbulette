e611e5969bce815bac4b94c8d03bc2bc7353e569
Gazorby <gazorby@pm.me>
2020-08-17 01:10:19 +0200

    ✅ test(auth): code refactoring & cleanup

--- tests/app_1/resolvers/user.py
+++ tests/app_1/resolvers/user.py
@@ -5,7 +5,7 @@ from turbulette.apps.auth.pyd_models import BaseUserCreate
 from turbulette.apps.auth.utils import create_user
 from turbulette.core.errors import BaseError
 from turbulette.core.validation.decorators import validate
-from turbulette.apps.auth.decorators import login_required
+from turbulette.apps.auth.decorators import access_token_required
 
 
 @mutation.field("createUser")
@@ -21,7 +21,6 @@ async def resolve_user_create(obj, info, valid_input, **kwargs) -> dict:
         return BaseError(f"User {user_data['username']} already exists").dict()
 
     new_user = await create_user(**user_data, permission_group="customer")
-    # new_fan = await BaseUser.create(**fan_data, user=new_user.id)
     auth_token = get_token_from_user(new_user)
     return {
         "user": {**new_user.to_dict()},
@@ -30,11 +29,32 @@ async def resolve_user_create(obj, info, valid_input, **kwargs) -> dict:
 
 
 @query.field("books")
-@login_required
 async def resolve_books(_, info, user, **kwargs):
     return {
         "books": [
             {"title": "Harry Potter", "author": "J.K Rowling"},
-            {"title": "The Lord of Rings", "author": "J.R.R Tolkien"},
+            {"title": "The Lord of the Rings", "author": "J.R.R Tolkien"},
+        ]
+    }
+
+@mutation.field("addBook")
+async def add_books(_, __, user, **kwargs):
+    return {
+        "success": True
+    }
+
+@mutation.field("borrowBook")
+async def borrow_book(_, __, user, **kwargs):
+    return {
+        "success": True
+    }
+
+
+@query.field("exclusiveBooks")
+@access_token_required
+async def is_logged(_, __, claims, **kwargs):
+    return {
+        "books": [
+            {"title": "Game Of Thrones", "author": "G.R.R Martin"},
         ]
     }
