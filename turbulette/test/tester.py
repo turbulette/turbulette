31d46a4481e12ea56940ed2d2dcdc08c1224a194
Gazorby <gazorby@pm.me>
2020-08-16 19:36:24 +0200

    ✨ feat(test): add method to test data in response

--- turbulette/test/tester.py
+++ turbulette/test/tester.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from ariadne import graphql
 from ariadne.types import GraphQLResult, GraphQLSchema
 
@@ -10,9 +11,12 @@ class Tester:
         self.schema = schema
 
     async def query(
-        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
+        self,
+        query: str,
+        variables: dict = None,
+        op_name: str = None,
+        headers: dict = None,
     ) -> GraphQLResult:
-
         class TestRequest:
             def __init__(self, headers: dict = None):
                 self.headers = headers
@@ -21,11 +25,15 @@ class Tester:
             self.schema,
             data={"query": query, "variables": variables, "operationName": op_name},
             context_value={"request": TestRequest(headers)},
-            debug=True
+            debug=True,
         )
 
     async def assert_query_success(
-        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
+        self,
+        query: str,
+        variables: dict = None,
+        op_name: str = None,
+        headers: dict = None,
     ) -> GraphQLResult:
         response = await self.query(query, variables, op_name, headers)
         self.assert_status_200(response)
@@ -33,7 +41,11 @@ class Tester:
         return response
 
     async def assert_query_failed(
-        self, query: str, variables: dict = None, op_name: str = None, headers: dict = None
+        self,
+        query: str,
+        variables: dict = None,
+        op_name: str = None,
+        headers: dict = None,
     ) -> GraphQLResult:
         response = await self.query(query, variables, op_name, headers)
         self.assert_status_200(response)
@@ -48,3 +60,18 @@ class Tester:
 
     def assert_errors(self, response: dict):
         assert "errors" in response[1]
+
+    @classmethod
+    def assert_data_in_response(
+        cls, response: GraphQLResult, query_name: str, data: Dict[str, Any]
+    ):
+        """Assert that the response contains the specified key with the corresponding values
+
+        Args:
+            response (GraphQLResult): Response to check
+            query_name (str): Name of the GraphQL query/mutation
+            data (Dict[str, Any]): Data that should be present in query response
+        """
+        for key, value in data.items():
+            if key in response[1]["data"][query_name]:
+                assert response[1]["data"][query_name][key] == value
