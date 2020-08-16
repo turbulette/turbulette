11cddc33c333855e3ac257fd2af2fe607231e56e
Gazorby <gazorby@pm.me>
2020-08-16 17:52:19 +0200

    ♻️ ref: remove unused imports to gino-enum-tables

--- tests/alembic/env.py
+++ tests/alembic/env.py
@@ -2,7 +2,6 @@ import sys
 from os.path import abspath, join
 from os import getcwd
 from turbulette.alembic.env import run_migrations
-from enumtables import alembic_ops
 
 
 # Add project folder to python path
