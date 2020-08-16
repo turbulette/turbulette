0bbe3158eb032285ddb1bef658b5584881deef7e
Gazorby <gazorby@pm.me>
2020-08-16 23:30:16 +0200

    ♻️ ref(auth): remove unused exception

--- turbulette/apps/auth/exceptions.py
+++ turbulette/apps/auth/exceptions.py
@@ -1,14 +1,3 @@
-
-class PermissionGroupNotFound(Exception):
-    default_message = None
-
-    def __init__(self, message=None):
-        if message is None:
-            message = self.default_message
-
-        super().__init__(message)
-
-
 class JSONWebTokenError(Exception):
     default_message = "An error occured while processing the JSON web token"
 
