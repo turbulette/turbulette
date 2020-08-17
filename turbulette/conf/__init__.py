5615a48f94c7da054e125d96d996b7a7506664dc
Gazorby <gazorby@pm.me>
2020-08-17 19:28:59 +0200

    ♻️ ref(conf): remove unused code

--- turbulette/conf/__init__.py
+++ turbulette/conf/__init__.py
@@ -1,7 +1,4 @@
-import sys
 from turbulette.conf.constants import OVERRIDE_BY_ENV
-from types import ModuleType
-from .exceptions import NotReadyError
 from . import constants
 from .utils import get_config_from_paths
 
@@ -16,39 +13,3 @@ SIMPLE_SETTINGS = {
     constants.SETTINGS_LOGS: False,
     constants.OVERRIDE_BY_ENV: True
 }
-
-
-# class ConfModule(ModuleType):
-#     """Manage configuration resources access
-#     """
-
-#     def __init__(self, name):
-#         super(ConfModule, self).__init__(name)
-#         self.simple_settings = {
-#             constants.REQUIRED_SETTINGS: set(),
-#             constants.REQUIRED_SETTINGS_TYPES: {},
-#             constants.REQUIRED_NOT_NONE_SETTINGS: set(),
-#             constants.SETTINGS_LOGS: False,
-#         }
-#         self.registry = None
-#         self.db = None
-#         self.settings = None
-
-#     def __getattribute__(self, attr):
-#         val = object.__getattribute__(self, attr)
-#         # Attributes may want to be accessed before their initialization
-#         if val is None:
-#             raise NotReadyError(attr)
-#         return val
-
-#     # def __setattr__(self, name, value):
-#     #     # Attributes can only be set once
-#     #     # This guarantee that all references on any of the class attributes
-#     #     # will give access to the same object, from the moment it exists
-#     #     if not self.__dict__.get(name):
-#     #         self.__dict__[name] = value
-#     #     else:
-#     #         raise ValueError(f"{name} : This attribute can only be set once")
-
-# _conf_module = ConfModule(__name__)
-# sys.modules[__name__] = _conf_module
