cce786e167f91023acd723944480c40707649714
Gazorby <gazorby@pm.me>
2020-08-17 19:29:19 +0200

    ♻️ ref(conf): remove unused exception

--- turbulette/conf/exceptions.py
+++ turbulette/conf/exceptions.py
@@ -1,9 +1,3 @@
-class NotReadyError(Exception):
-    """A resource isn't created yet"""
-    def __init__(self, resource: str):
-        self.resource = resource
-        super().__init__(f"{resource} isn't ready yet")
-
 class ImproperlyConfigured(Exception):
     """Trubulette is somehow improperly configured"""
     def __init__(self, message):
