eec5846bc24dab0419bbdb02b25821c483abfd21
Gazorby <gazorby@pm.me>
2020-08-18 16:36:25 +0200

    👷‍♂️ chore(ci): add Travis CI

--- tests/alembic/env.py
+++ tests/alembic/env.py
@@ -1,9 +1,9 @@
 import sys
 from os.path import abspath, join
-from os import getcwd
+from os import getcwd, environ
 from turbulette.alembic.env import run_migrations
 
 
 # Add project folder to python path
 sys.path.append(abspath(join(getcwd())))
-run_migrations("tests.settings")
+run_migrations(environ["PYTEST_TURBULETTE_SETTINGS"])
