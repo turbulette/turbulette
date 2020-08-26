bf25822bf4004a362c3658c2c9f014a7fcc8133a
Gazorby <gazorby@pm.me>
2020-08-27 00:32:41 +0200

    ✅ test: add tests
    We use async-asgi-testclient as test client, and python-multipart to test request form

---
+++ tests/routing.py
@@ -0,0 +1,12 @@
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+from turbulette.routes import csrf
+
+
+async def welcome(request):
+    return JSONResponse({"welcome": "welcome to the library"})
+
+routes = [
+    Route("/csrf", endpoint=csrf),
+    Route("/welcome", endpoint=welcome, methods=["GET", "POST"])
+]
