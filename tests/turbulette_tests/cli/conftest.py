9325ed1f9361d809be86b1b9f2002a7e8e951000
Gazorby <gazorby@pm.me>
2020-08-28 14:40:32 +0200

    ✅ test(cli): imrove tests

--- tests/turbulette_tests/cli/conftest.py
+++ tests/turbulette_tests/cli/conftest.py
@@ -1,20 +1,53 @@
+import sys
 from datetime import datetime
 from importlib import import_module
+import contextlib
 from pathlib import Path
 from shutil import rmtree
-
+from os import chdir
 import pytest
+from click.testing import CliRunner
 from gino import create_engine
+from tempfile import TemporaryDirectory
+
+from turbulette.core.management.cli import cli
 
 PROJECT = "__test_project"
 APP_1 = "__test_app_1"
 APP_2 = "__test_app_2"
+APP_3 = "__test_app_3"
 
 
-def pytest_sessionfinish(session, exitstatus):
-    project_dir = Path.cwd() / PROJECT
-    if project_dir.is_dir():
-        rmtree(project_dir.as_posix())
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    chdir(path)
+    try:
+        yield
+    finally:
+        chdir(prev_cwd)
+
+
+@pytest.fixture(scope="session")
+def create_project():
+    tmp_dir = TemporaryDirectory()
+    with working_directory(tmp_dir.name):
+        runner = CliRunner()
+        res = runner.invoke(cli, ["create-project", "--name", PROJECT])
+        assert res.exit_code == 0
+    yield Path(tmp_dir.name) / PROJECT
+    tmp_dir.cleanup()
+
+
+@pytest.fixture(scope="session")
+def create_apps(create_project):
+    runner = CliRunner()
+    chdir(create_project)
+    res = runner.invoke(
+        cli, ["create-app", "--name", APP_1, "--name", APP_2, "--name", APP_3]
+    )
+    assert res.exit_code == 0
 
 
 @pytest.fixture(scope="session")
@@ -23,30 +56,38 @@ def db_name_cli():
 
 
 @pytest.fixture(scope="session")
-def project_settings_cli():
-    return import_module(f"{PROJECT}.settings")
+def project_settings_cli(create_env, create_project):
+    sys.path.insert(1, create_project.parent.as_posix())
+    with working_directory(create_project):
+        return import_module(f"{PROJECT}.settings")
 
 
 @pytest.fixture(scope="session")
-def create_env(db_name_cli):
-    env_file = (Path.cwd() / PROJECT / ".env").open("w")
-    env_file.writelines([
-        "DB_DRIVER=postgresql"
-        "DB_HOST=localhost"
-        "DB_PORT=5432"
-        "DB_USER=postgres"
-        "DB_PASSWORD=\"\""
-        f"DB_DATABASE={db_name_cli}"
-    ])
+def create_env(db_name_cli, create_project):
+    env_file = (create_project / ".env").open("w")
+    env_file.write(
+        "\n".join(
+            [
+                "DB_DRIVER=postgresql",
+                "DB_HOST=localhost",
+                "DB_PORT=5432",
+                "DB_USER=postgres",
+                'DB_PASSWORD=""',
+                f"DB_DATABASE={db_name_cli}",
+            ]
+        )
+    )
     env_file.close()
 
 
 @pytest.fixture(scope="session")
-async def create_db_cli(db_name_cli, project_settings_cli, create_env, request):
+async def create_db_cli(db_name_cli, project_settings_cli, request):
     # Connect to the default template1 database to create a new one
     project_settings_cli.DB_DSN.database = "template1"
     # The pool must be able to authorize two connection to drop the test db if needed
-    engine = await create_engine(str(project_settings_cli.DB_DSN), min_size=1, max_size=2)
+    engine = await create_engine(
+        str(project_settings_cli.DB_DSN), min_size=1, max_size=2
+    )
     async with engine.acquire():
         await engine.status(f'CREATE DATABASE "{db_name_cli}"')
         project_settings_cli.DB_DSN.database = db_name_cli
--- tests/turbulette_tests/cli/test_cli.py
+++ tests/turbulette_tests/cli/test_cli.py
@@ -1,57 +1,60 @@
-import os
-from os import chdir
-from pathlib import Path
 from tempfile import TemporaryDirectory
 from click.testing import CliRunner
 from turbulette.core.management.cli import cli
 
-from .conftest import APP_1, APP_2, PROJECT
+from .conftest import APP_1, APP_2, APP_3, working_directory
 
 
 def test_create_project():
     runner = CliRunner()
-    res = runner.invoke(cli, ["create-project", "--name", PROJECT])
-    assert res.exit_code == 0
+    with TemporaryDirectory() as tmp_dir:
+        with working_directory(tmp_dir):
+            res = runner.invoke(cli, ["create-project", "--name", "project_1"])
+            assert res.exit_code == 0
+            res = runner.invoke(
+                cli,
+                [
+                    "create-project",
+                    "--name",
+                    "project_2",
+                    "--app",
+                    "app_1",
+                    "--app",
+                    "app_2",
+                ],
+            )
+            assert res.exit_code == 0
 
 
 def test_not_a_project_dir():
     runner = CliRunner()
-    old = Path.cwd()
-    tmp_dir = TemporaryDirectory()
-    chdir(tmp_dir.name)
-    res = runner.invoke(cli, ["create-app", "--name", APP_1])
-    assert res.exit_code != 0
-    tmp_dir.cleanup()
-    os.chdir(old)
+    with TemporaryDirectory() as tmp_dir:
+        with working_directory(tmp_dir):
+            res = runner.invoke(cli, ["create-app", "--name", "app_1"])
+            assert res.exit_code != 0
 
 
-def test_create_app():
-    old = Path.cwd()
-    os.chdir(PROJECT)
-    runner = CliRunner()
-    res = runner.invoke(cli, ["create-app", "--name", APP_1])
-    assert res.exit_code == 0
-    res = runner.invoke(cli, ["create-app", "--name", APP_2])
-    assert res.exit_code == 0
-    os.chdir(old)
+def test_create_app(create_project):
+    with working_directory(create_project):
+        runner = CliRunner()
+        res = runner.invoke(cli, ["create-app", "--name", "app_1"])
+        assert res.exit_code == 0
+        res = runner.invoke(cli, ["create-app", "--name", "app_2", "--name", "app_3"])
+        assert res.exit_code == 0
 
 
-def test_upgrade(create_db_cli):
-    runner = CliRunner()
-    old = Path.cwd()
-    os.chdir(PROJECT)
-    res = runner.invoke(cli, ["upgrade", APP_1])
-    assert res.exit_code == 0
-    # Should upgrade APP_2
-    res = runner.invoke(cli, ["upgrade"])
-    assert res.exit_code == 0
-    os.chdir(old)
+def test_upgrade(create_project, create_db_cli, create_apps):
+    with working_directory(create_project):
+        runner = CliRunner()
+        res = runner.invoke(cli, ["upgrade", APP_1])
+        assert res.exit_code == 0
+        # Should upgrade APP_2 and APP_3
+        res = runner.invoke(cli, ["upgrade"])
+        assert res.exit_code == 0
 
 
-def test_makerevision(create_db_cli):
-    runner = CliRunner()
-    old = Path.cwd()
-    os.chdir(PROJECT)
-    res = runner.invoke(cli, ["makerevision", APP_1])
-    assert res.exit_code == 0
-    os.chdir(old)
+def test_makerevision(create_project, create_db_cli, create_apps):
+    with working_directory(create_project):
+        runner = CliRunner()
+        res = runner.invoke(cli, ["makerevision", APP_2])
+        assert res.exit_code == 0
