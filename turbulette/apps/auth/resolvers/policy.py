00277ec2ca5755e241530ed0b0ced5e40393b40a
Gazorby <gazorby@pm.me>
2020-09-21 20:49:53 +0200

    ♻️ refactor: silent some pylint warnings

--- turbulette/apps/auth/resolvers/policy.py
+++ turbulette/apps/auth/resolvers/policy.py
@@ -7,22 +7,24 @@ from turbulette.apps.auth import policy
 
 @policy.condition("claim")
 async def claim(
-    val, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val, claims: Claims, info: GraphQLResolveInfo  # pylint: disable=unused-argument
+) -> bool:
     return all(inc in claims[val["name"]] for inc in val["includes"])
 
 
 @policy.condition("is_claim_present")
 async def is_claim_present(
-    val, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val, claims: Claims, info: GraphQLResolveInfo  # pylint: disable=unused-argument
+) -> bool:
     return val in claims
 
 
 @policy.principal("perm")
 async def has_perm(
-    val: str, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val: str,
+    claims: Claims,
+    info: GraphQLResolveInfo,  # pylint: disable=unused-argument
+) -> bool:
     involved = False
     for role in claims["scopes"]:
         if not role.startswith("_"):
@@ -35,22 +37,28 @@ async def has_perm(
 
 @policy.principal("staff")
 async def is_staff(
-    val: str, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val: str,  # pylint: disable=unused-argument
+    claims: Claims,
+    info: GraphQLResolveInfo,  # pylint: disable=unused-argument
+) -> bool:
     return STAFF_SCOPE in claims["scopes"]
 
 
 @policy.principal("role")
 async def has_role(
-    val: str, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val: str,
+    claims: Claims,
+    info: GraphQLResolveInfo,  # pylint: disable=unused-argument
+) -> bool:
     return val in claims["scopes"]
 
 
 @policy.principal("user")
 async def is_user(
-    val: str, claims: Claims, info: GraphQLResolveInfo
-) -> bool:  # pylint: disable=unused-argument
+    val: str,
+    claims: Claims,
+    info: GraphQLResolveInfo,  # pylint: disable=unused-argument
+) -> bool:
     return claims["sub"] == val
 
 
