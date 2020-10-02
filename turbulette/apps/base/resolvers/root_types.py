3b52faa3be7424c820bcaed1d9ff5f7a6428fc70
Gazorby <gazorby@pm.me>
2020-10-03 01:20:11 +0200

    ♻️ refactor(scalars): remove unneeded decorator

--- turbulette/apps/base/resolvers/root_types.py
+++ turbulette/apps/base/resolvers/root_types.py
@@ -1,10 +1,9 @@
 import json
 from datetime import datetime
-from typing import Any, Callable
+from typing import Any
 
 import ciso8601
 from ariadne import MutationType, QueryType, ScalarType
-from turbulette import conf
 
 query = QueryType()
 mutation = MutationType()
@@ -17,33 +16,17 @@ json_scalar = ScalarType("JSON")
 base_scalars_resolvers = [datetime_scalar, date_scalar, json_scalar]
 
 
-def _extract_value(func: Callable[[Any], str]) -> Callable[[Any], str]:
-    """If `value` is a dict, return the first item.
-    When a query return a custom scalar, it is wrapped in a dict
-    so we have to extract it before the serialization.
-    """
-
-    def wrapped(value: Any):
-        if isinstance(value, dict) and conf.settings.ERROR_FIELD not in value:
-            value = value[next(iter(value))]
-        return func(value)
-
-    return wrapped
-
-
 @datetime_scalar.value_parser
 def parse_datetime(value: str) -> datetime:
     return ciso8601.parse_datetime(value)
 
 
 @datetime_scalar.serializer
-@_extract_value
 def serialize_datetime(value: Any) -> str:
     return value.isoformat()
 
 
 @date_scalar.serializer
-@_extract_value
 def serialize_date(value: Any) -> str:
     return value.strftime("%Y-%m-%d")
 
