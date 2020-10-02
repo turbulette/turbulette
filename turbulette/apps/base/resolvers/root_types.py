062e020479d1577d3e86765c842270021e8b0fa0
Gazorby <gazorby@pm.me>
2020-10-02 20:34:47 +0200

    🐛 fix(scalar): fix date/datetime serializer

--- turbulette/apps/base/resolvers/root_types.py
+++ turbulette/apps/base/resolvers/root_types.py
@@ -1,9 +1,10 @@
 import json
 from datetime import datetime
-from typing import Any
+from typing import Any, Callable
 
 import ciso8601
 from ariadne import MutationType, QueryType, ScalarType
+from turbulette import conf
 
 query = QueryType()
 mutation = MutationType()
@@ -16,18 +17,34 @@ json_scalar = ScalarType("JSON")
 base_scalars_resolvers = [datetime_scalar, date_scalar, json_scalar]
 
 
+def _extract_value(func: Callable[[Any], str]) -> Callable[[Any], str]:
+    """If `value` is a dict, return the first item.
+    When a query return a custom scalar, it is wrapped in a dict
+    so we have to extract it before the serialization.
+    """
+
+    def wrapped(value: Any):
+        if isinstance(value, dict) and conf.settings.ERROR_FIELD not in value:
+            value = value[next(iter(value))]
+        return func(value)
+
+    return wrapped
+
+
 @datetime_scalar.value_parser
 def parse_datetime(value: str) -> datetime:
     return ciso8601.parse_datetime(value)
 
 
 @datetime_scalar.serializer
-def serialize_datetime(value: datetime) -> str:
+@_extract_value
+def serialize_datetime(value: Any) -> str:
     return value.isoformat()
 
 
 @date_scalar.serializer
-def serialize_date(value: datetime) -> str:
+@_extract_value
+def serialize_date(value: Any) -> str:
     return value.strftime("%Y-%m-%d")
 
 
