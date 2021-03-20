2e1ca69d3a153c8e750ef7f107f8e76fd48b115a
Gazorby <gazorby@pm.me>
2021-03-20 14:32:17 +0100

    👷 build: delete depreceated script

--- scripts/install-poetry.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -x
-set -e
-
-pip install pip==20.3.1 setuptools==50.3.2
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-source $HOME/.poetry/env
