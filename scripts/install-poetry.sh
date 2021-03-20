88e33e5b1641e272d39423b3953f4116e5c791b3
Gazorby <gazorby@pm.me>
2021-03-20 01:39:24 +0100

    💚 ci: fix poetry install

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -4,6 +4,6 @@ set -x
 set -e
 
 pip install pip==20.3.1 setuptools==50.3.2
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -fsS -o get-poetry.py https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py
 python get-poetry.py -y
 source $HOME/.poetry/env
